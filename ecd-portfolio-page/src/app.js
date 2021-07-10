c86a5204f7323408e16f58b69f773534e133d1b1
desantel <ecdesantis21@gmail.com>
2021-07-10 15:51:44 -0400

    app, and index.js

---
+++ ecd-portfolio-page/src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from './components/Header';
+import Projects from './components/Projects';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Header />
+            <Projects />
+            <Footer />
+        </div>
+    )
+}
