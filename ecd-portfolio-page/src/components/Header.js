36d63a7c5c809ddaff332d79ff8f74e6db9509bb
desantel <ecdesantis21@gmail.com>
2021-07-10 18:50:30 -0400

    adding in design

--- ecd-portfolio-page/src/components/Header.js
+++ ecd-portfolio-page/src/components/Header.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import '../styles/Header.css';
+
+const styles = {
+    headerStyle: {
+        background: 'Twilight Lavender'
+    },
+    headingStyle: {
+        color: 'Celadon',
+        fontSize: '100px'
+    }
+},
+
+function Header() {
+    return(
+        <header style = {styles.headerStyle} className="header">
+            <h1 style={styles.headingStyle}>
+                Elizabeth De Santis Portfolio
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
